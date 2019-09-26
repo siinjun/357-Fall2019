ed695b5af3045db63c907ee8d718957ee2d9ff42
Sinjun Jang <srjang@unix3.csc.calpoly.edu>
2019-09-26 15:49:48 -0700

    fixed detab func

--- assignment01/detab.c
+++ assignment01/detab.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 #define TABSTOP 8
+#define TRUE 1
+#define FALSE 0
 
 void detab()
 {
 
     int ch_count = 0;
     int ch = getchar();
+    int beginning_of_line = TRUE;
 
     while (ch != EOF) /*while not end of file*/
     {
@@ -13,21 +16,25 @@ void detab()
 
         if (ch == '\t') /* if current char is tab, add spaces until tabstop*/
         {
-            while ((ch_count % TABSTOP) != 0){
+            beginning_of_line = FALSE;
+            for (;ch_count <= TABSTOP; ch_count++)
+            {
                 putchar(' ');
-                ch_count++;
             }
+            ch_count = 0;
+
         }
 
         else if (ch == '\n' || ch == '\r') /*if current char is newline, reset count to 0*/
         {
             ch_count = 0;
             putchar(ch);
+            beginning_of_line = TRUE;
         }
 
         else if (ch == '\b') /*if current char is backspace, decrement count*/
         {
-            if (ch_count > 0){
+            if (beginning_of_line == FALSE){
 
             ch_count--;
             putchar(ch);
@@ -37,10 +44,14 @@ void detab()
  
         else /*print all normal characters*/
         {
+            beginning_of_line = FALSE;
             putchar(ch);
         }
 
         ch = getchar(); /*get next char in file*/
+
+        if (ch_count == TABSTOP)
+            ch_count = 0;
     }
 }
 
