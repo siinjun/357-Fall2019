9829f946fb27a2843589fda66610682e9d060b4a
Sinjun Jang <sinjunjang@gmail.com>
2019-09-30 15:40:21 -0700

    fixed detab cause of edge cases

--- assignment01/detab.c
+++ assignment01/detab.c
@@ -1,23 +1,19 @@
 #include<stdio.h>
 #define TABSTOP 8
-#define TRUE 1
-#define FALSE 0
 
 void detab()
 {
 
     int ch_count = 0;
     int ch = getchar();
-    int beginning_of_line = TRUE;
+    int beginning_of_line = 0;
 
     while (ch != EOF) /*while not end of file*/
     {
-        ch_count++; /*increment the char count*/
 
         if (ch == '\t') /* if current char is tab, add spaces until tabstop*/
         {
-            beginning_of_line = FALSE;
-            for (;ch_count <= TABSTOP; ch_count++)
+            for (;ch_count < TABSTOP; ch_count++, beginning_of_line++)
             {
                 putchar(' ');
             }
@@ -29,28 +25,29 @@ void detab()
         {
             ch_count = 0;
             putchar(ch);
-            beginning_of_line = TRUE;
+            beginning_of_line = 0;
         }
 
         else if (ch == '\b') /*if current char is backspace, decrement count*/
         {
-            if (beginning_of_line == FALSE){
-
+            if (beginning_of_line != 0)
+            {
+            beginning_of_line--;
             ch_count--;
             putchar(ch);
-
             }
         }
  
         else /*print all normal characters*/
         {
-            beginning_of_line = FALSE;
+            beginning_of_line++;
+            ch_count++;
             putchar(ch);
         }
 
         ch = getchar(); /*get next char in file*/
 
-        if (ch_count == TABSTOP)
+        if (ch_count == TABSTOP) /*if char counter equals tabstop reset it*/
             ch_count = 0;
     }
 }
@@ -60,4 +57,4 @@ int main ()
     detab();
 
     return 0;
-}
+
