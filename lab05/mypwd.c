11b7e344b50c893e5f42ac5b5a13b83a47428d55
Sinjun Jang <sinjunjang@gmail.com>
2019-10-29 21:56:07 -0700

    damn this one hurt

--- lab05/mypwd.c
+++ lab05/mypwd.c
@@ -1,123 +1,94 @@
 #include<sys/types.h>
-#include<limits.h>
-#include<dirent.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<stdbool.h>
+#include<string.h>
+#include<dirent.h>
 #include<errno.h>
+#define PATH_MAX 2048
+bool DEBUG = false;
 
-#define BUFF 40
 
-int contents_child=0;
-int contents_par= 0;
 
-char **get_contents(DIR *dir){
-    /*gets contents of curr dir*/
-    char **contents;
-    int buff = BUFF,i=0, count=0;
+char *mypwd(char *path, char *pwd){
+
+    struct stat *current=NULL, *parent=NULL;
+    int valid, len;
+    char *tmp, *name, *root = "/\0";
+    DIR *cd;
     struct dirent *de;
 
-    contents = calloc(1, buff);
-    while((de=readdir(dir)) != 0){
-        count+=8;
-        if(count >= buff){
-            contents = realloc(contents, buff * 2);
-            if(!contents){
-                perror("Realloc");
-                exit(1);
-            }
-            buff *= 2;
-        }
-        contents[i++] = de->d_name;
-    }
-    contents_child = i;
-    return contents;
+    current = calloc(1, sizeof(struct stat));
+    parent = calloc(1, sizeof(struct stat));
+    tmp = calloc(PATH_MAX, sizeof(char));
 
-}
-char **get_par_contents(DIR *dir){
-    /*gets contents of parent dir and its sub dirs*/
-    char **contents;
-    int buff = BUFF,i=0, count=0;
-    struct dirent *de;
-
-    contents = calloc(1, buff);
-    while((de=readdir(dir)) != 0){
-        count+=8;
-        if(count >= buff){
-            contents = realloc(contents, buff * 2);
-            if(!contents){
-                perror("Realloc");
-                exit(1);
-            }
-            buff *= 2;
-        }
-        contents[i++] = de->d_name;
-    }
-    contents_par = i;
-    return contents;
-
-}
-
-bool compare_contents(char**one, char **two){
-
-    int i =0;
-    if(contents_par != contents_child){
-        return false;
-    }
-    else{
-        for(;i < contents_par;i++){
-            if(strcmp(one[i], two[i]))
-                return false;
-        }
-        return true;
-    }
-}
-
-void find_parent(const char *parent, char **contents){
-    /*returns name of child, or NULL if at root*/
-    char *name;
-    char **new_con=NULL;
-
-}
-
-void getpwd(const char *cd, const char *prev_d){
-
-    DIR *dir;
-    char **contents=NULL;
-    char *path_to_root=NULL;
-    char *prev = "../\0";
-    int i=0, buff = 8;
-
-    path_to_root = malloc(buff);
-    if(!path_to_root){
-        perror("malloc");
+    valid = stat(path, current);
+    if(valid < 0){
+        perror("stat()");
         exit(1);
     }
-    /*reads all files in directory*/
-    dir = opendir(cd);
-    /*if dir is not a directory*/
-    if(errno == ENOENT)
-        perror(cd);
-    else{
-        contents = get_contents(dir);
-        i += 3;
-        if(i>=buff){
-            path_to_root = realloc(path_to_root, buff*2);
-            if(!path_to_root){
-                perror("malloc");
-                exit(1);
-            }
-        }
-        path_to_root = strcat(path_to_root, prev);
-        find_parent(path_to_root, contents);
+    path = strcat(path, "../\0");
+    valid = stat(path, parent);
+    if(valid < 0){
+        perror("stat()");
+        exit(1);
     }
+    if(parent->st_dev == current->st_dev){
+        cd = opendir(path);
+        if(cd == NULL){
+            perror("opendir()");
+            exit(1);
+        }
+        while((de=readdir(cd)) != NULL){
+            name = de->d_name;
+            tmp = strcat(tmp, path);
+            tmp = strcat(tmp, name);
+            valid = stat(tmp, parent);
+            if(parent->st_ino == current->st_ino &&
+                    (!strcmp(name, "..") || !strcmp(name, ".")))
+                break;
+            if(parent->st_dev == current->st_dev &&
+                    parent->st_ino == current->st_ino){
+                pwd = mypwd(path, pwd);
+                pwd = strcat(pwd, root);
+                pwd = strcat(pwd, name);
+                closedir(cd);
+                free(tmp);
+                free(current);
+                free(parent);
+                return pwd;
+            }
+
+            len = strlen(tmp);
+            memset(tmp, 0, len);
+
+        }
+    }
+    free(current);
+    free(parent);
+    free(tmp);
+    closedir(cd);
+    pwd = strcat(pwd, root);
+    return pwd;
 }
+int main(){
 
-
-int main(int argc, char *argv[]){
-
-    char *currdir = ".";
-    getpwd(currdir, NULL);
+    char *cd;
+    char *pwd;
+    pwd = calloc(PATH_MAX, sizeof(char));
+    cd = calloc(PATH_MAX, sizeof(char));
+    cd[0] = '.';
+    cd[1] = '/';
+    cd[2] = '\0';
+    /*
+    path = getpwd(cd, path);
+    */
+    pwd = mypwd(cd, pwd);
+    printf("%s\n", pwd);
+    free(cd);
+    free(pwd);
     return 0;
 }
