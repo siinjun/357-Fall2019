0f6f1b3683c4f226b8ebf3f55f49228dcdbdc66f
Sinjun Jang <sinjunjang@gmail.com>
2019-11-06 17:15:56 -0800

    done

--- lab06/timeit.c
+++ lab06/timeit.c
@@ -26,35 +26,37 @@ int main(int argc, char * argv[]){
     int repeat;
     int num = 0;
 
-    repeat = 2 * strtol(argv[1],&endptr,10);
 
-    if(*endptr != '\0'){
-        printf("%s: malformed time.\n", argv[1]);
-        valid = false;
-    }
-    if(argc > 2)
+    if(argc > 2 || argc == 1)
         valid = false;
     if(valid){
-        sigemptyset(&mask);
-        sigaddset(&mask, SIGALRM);
-        sigprocmask(SIG_BLOCK,&mask,NULL);
-        sa.sa_handler = handler;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = 0;
-        sigaction(SIGALRM,&sa,NULL);
+        repeat = 2 * strtol(argv[1],&endptr,10);
+        if(*endptr != '\0'){
+            printf("%s: malformed time.\n", argv[1]);
+            valid = false;
+        }
+        if(valid){
+            sigemptyset(&mask);
+            sigaddset(&mask, SIGALRM);
+            sigprocmask(SIG_BLOCK,&mask,NULL);
+            sa.sa_handler = handler;
+            sigemptyset(&sa.sa_mask);
+            sa.sa_flags = 0;
+            sigaction(SIGALRM,&sa,NULL);
 
-        timer.it_value.tv_sec = 0;
-        timer.it_value.tv_usec = 500000;
-        timer.it_interval.tv_sec = 0;
-        timer.it_interval.tv_usec = 500000;
-        setitimer(ITIMER_REAL, &timer, NULL);
+            timer.it_value.tv_sec = 0;
+            timer.it_value.tv_usec = 500000;
+            timer.it_interval.tv_sec = 0;
+            timer.it_interval.tv_usec = 500000;
+            setitimer(ITIMER_REAL, &timer, NULL);
 
-        sigprocmask(SIG_UNBLOCK,&mask,NULL);
-        sigdelset(&mask,SIGALRM);
-        while(num++ < repeat)
-            pause();
+            sigprocmask(SIG_UNBLOCK,&mask,NULL);
+            sigdelset(&mask,SIGALRM);
+            while(num++ < repeat)
+                pause();
 
-        write(0,"Time's up!\n",11);
+            write(0,"Time's up!\n",11);
+        }
     }
     else
         printf("usage: timeit <seconds>\n");
