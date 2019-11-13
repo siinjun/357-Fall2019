abb94bdcb295b832ee354ed10158d903fd3a3017
Sinjun Jang <sinjunjang@gmail.com>
2019-11-12 17:00:07 -0800

    nearly done with table

--- assignment04/table.c
+++ assignment04/table.c
@@ -15,7 +15,7 @@
 #include "header.h"
 #include "ustarformat.c"
 #define BLOCK 512
-int V_FLG = 0;
+
 int str_2oct(char *octal){
     /*FIXME middle nulls*/
     int i = 0, num=0;
@@ -84,7 +84,6 @@ char *get_perms(struct header head){
         perms[8] = 'w';
     if(S_IXOTH & mode)
         perms[9] = 'x';
-    printf("perms a4: %s\n", perms);
     return perms;
 }
 
@@ -95,52 +94,125 @@ int get_size(struct header head){
     return size;
 }
 
-void read_v_headers(int fd){
+char *get_name(struct header hd){
+
+    char *name;
+
+    name = calloc(257,1);
+
+    if(hd.prefix[0]){
+        name = strncpy(name, hd.prefix, 155);
+        name = strncat(name, hd.name,100);
+    }
+    else{
+        name = strncat(name, hd.name,100);
+    }
+
+    return name;
+}
+
+char *get_time(struct header head){
+
+    char *mtime;
+    long int secs;
+    struct tm *time;
+
+    mtime = calloc(17,1);
+    secs = (long)str_2oct(head.mtime);
+    time = localtime(&secs);
+
+    strftime(mtime, 17, "%Y-%m-%d %H:%M", time);
+    return mtime;
+}
+
+char *create_verbose(char *perms,char *filename,int size,struct header head){
+    char *bytes, *verbose, *owner, *space;
+
+    int buf, pad;
+
+    bytes = calloc(8,1);
+    sprintf(bytes, "%u", size);
+
+    buf = 256 + 10 + 17 +8 +16;
+
+    /*the padding between owner and size*/
+    pad = 8 - strlen(bytes) + 1;
+    space = calloc(pad,1);
+    memset(space,32, pad-1);
+
+    owner = calloc(17, 1);
+    owner = strcpy(owner, head.uname);
+    owner = strcat(owner, "/");
+    owner = strcat(owner, head.gname);
+
+    verbose = calloc(buf, 1);
+
+    verbose = strncpy(verbose, perms, 10);
+
+    verbose = strncat(verbose, " ", 1);
+
+    verbose = strncat(verbose, owner, 17);
+
+    verbose = strncat(verbose, space,pad);
+
+    verbose = strncat(verbose, " ", 1);
+
+    verbose = strncat(verbose, bytes, 8);
+
+    verbose = strncat(verbose, " ", 1);
+
+    verbose = strncat(verbose, get_time(head), 16);
+
+    verbose = strncat(verbose, " ", 1);
+
+    verbose = strcat(verbose, filename);
+
+    return verbose;
+}
+
+void read_v_headers(int fd, int v_flg){
     struct header hd;
-    char *perms, *owner, *mtime, *filename;
+    char *perms, *filename, *verbose;
     bool done = false;
     int blocks, empty=0, size;
     while(!done){
         /*get tar header into struct header*/
-        owner = calloc(17, 1);
         read(fd, &hd, sizeof(struct header));
         lseek(fd, 12, SEEK_CUR);
         if(hd.name[0]){
             empty = 0;
             perms = get_perms(hd);
             size = get_size(hd);
-            owner = strcpy(owner, hd.uname);
-            owner = strcat(owner, "/");
-            owner = strcat(owner, hd.gname);
-            printf("owner: %s\n", owner);
+            filename = get_name(hd);
+            if(v_flg){
+                verbose = create_verbose(perms,filename,size,hd);
+                printf("%s\n", verbose);
+                fflush(stdout);
+            }
+            if(!v_flg){
+                printf("%s\n", filename);
+            }
             if(perms[0] != 'd' && perms[0] != 'l'){
                 blocks = size / BLOCK;
                 lseek(fd, (blocks+1)*BLOCK, SEEK_CUR);
             }
+            free(perms);
         }
         else{
             if(++empty == 2)
                 done = true;
         }
-        free(owner);
     }
 }
 
-void read_header(int fd){
-    struct header hd;
-    bool done = false;
-
-
-
-}
 
 int main(int argc, char *argv[]){
 
-    int fd;
+    int fd, v_flg=1;
 
     /*FIXME*/
-    fd = open(argv[1], O_RDONLY, 0644);
-    read_v_headers(fd);
+    fd = open("TestDir.tar", O_RDONLY, 0644);
+    read_v_headers(fd,v_flg);
     return 0;
 
 }
