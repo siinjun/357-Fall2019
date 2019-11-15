2d90599b218233c8a1dd3e5229b7162688b74a4b
Sinjun Jang <sinjunjang@gmail.com>
2019-11-15 12:09:36 -0800

    header

---
+++ assignment04/mytar.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void extract(int readfile, char *name);
+void createnewfile(char buff[512], char namebuf[256], int readfile, int amount);
+int octtodec(int n);
