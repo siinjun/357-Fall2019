9f55bc0b3dc2b3f516d61a3afc33d4ff8ca58cdf
Sinjun Jang <sinjunjang@gmail.com>
2019-11-16 11:32:16 -0800

    extract

--- assignment04/functions.c
+++ assignment04/functions.c
@@ -1,60 +1,127 @@
+#include "mytar.h"
 #include "header.h"
 
 void extract(int readfile, char *name){
 	char buff[512];
 	char namebuf[256];
 	char size[12];
-	int remainder = 0;
 	int amount = 0;
-	int numblocks = 0;
 	int i, j;
-    struct header head;
+	struct header head;
 
 	memset(buff, 0 ,512);
-	memset(namebuf, 0 , 256);
-    read(readfile, &head, sizeof(struct header));
+
+	/*read header first*/
+	read(readfile, &head, sizeof(struct header));
+	lseek(readfile, 12, SEEK_CUR);
+
+	if(head.prefix[0] != 0){
+		namebuf = strncpy(namebuf, head.prefix, 155);
+		strcat(namebuf, "/");
+		strcat(namebuf, head.name);
+	}
+	else{
+		namebuf = strdup(head.name);
+	}
+
+	size = strdup(head.size);
+
+	/*octtodec = turns oct to dec*/
+	/*find out how much need to write*/
+	amount = octtodec(atoi(size));
+
 	if(name != NULL){
-		/*read header first*/
-		while(read(readfile, buff, 512)>0){
-			/*printf("header: %s\n", buff);*/
-			memset(namebuf, 0 ,256);
+		/*while loop should check namebuf if it is the correct file or not*/
+		/*if not correct one, move on to next file*/
+		while(strcmp(name,namebuf) != 0){
+			lseek(readfile, amount, SEEK_CUR);
+			lseek(readfile, 512 - amount % 512, SEEK_CUR);
 
-			/*read name first*/
-			for(i = 0; i< 100; i++){
-				namebuf[i] = buff[i];
+			/*move on to the next file*/
+			read(readfile, &head, sizeof(struct header));
+			lseek(readfile, 12, SEEK_CUR);
+
+			if(head.prefix[0] != 0){
+					namebuf = strncpy(namebuf, head.prefix, 155);
+					strcat(namebuf, "/");
+					strcat(namebuf, head.name);
 			}
+			else{
+				namebuf = strdup(head.name);
+			}
+			size = strdup(head.size);
+			amount = octtodec(atoi(size));
 
+			/*printf("header: %s\n", buff);*/
+			/*read name first*/
+			/*for(i = 0; i< 100; i++){
+				namebuf[i] = buff[i];
+			}*/
 			/*handle if name longer than 100 char*/
-			if(namebuf[99]!= '\0'){
+			/*if(namebuf[99]!= '\0'){
 				j = 100;
 				i = 345;
-
 				while(buff[i] != '\0' && i< 500){
 					namebuf[j] = buff[i];
 					i++;
 					j++;
 				}
-
 				namebuf[j] = '\0';
-			}
-			/*printf("%s\n",namebuff);*/
+			}*/
+			/*printf("%s\n",namebuf);*/
 			/*printf("%s\n",name);*/
-
-			for(i = 0, j = 124; i<12; i++, j++){
+			/*for(i = 0, j = 124; i<12; i++, j++){
 				size[i] = buff[j];
-			}
+			}*/
+		}
+		createnewfile(readfile, namebuf, amount);
 
-			/*octtodec = turns oct to dec*/
-			/*find out how much need to write*/
+
+	}else{
+		while(namebuf[0] != 0){
+			/*writing contents of new file*/
+			createnewfile(readfile, namebuf, amount);
+			lseek(readfile, 512 - amount % 512, SEEK_CUR);
+
+			/*getting new info*/
+			/*get to new header first*/
+			read(readfile, &head, sizeof(struct header));
+			lseek(readfile, 12, SEEK_CUR);
+
+			if(head.prefix[0] != 0){
+					namebuf = strncpy(namebuf, head.prefix, 155);
+					strcat(namebuf, "/");
+					strcat(namebuf, head.name);
+			}
+			else{
+				namebuf = strdup(head.name);
+			}
+			size = strdup(head.size);
 			amount = octtodec(atoi(size));
 
-			if(namebuf[0]!= 0)/*&& strcmp(name,namebuf)== 0)*/{
-				/*printf("%s\n",namebuff);*/
+			/*checks first time if namebuf is empty*/
+			/*if namebuf is empty it checks the next block
+			to see if that's empty too*/
+			if(namebuf[0] == 0){
+				read(readfile, &head, sizeof(struct header));
+				lseek(readfile, 12, SEEK_CUR);
+
+				if(head.prefix[0] != 0){
+						namebuf = strncpy(namebuf, head.prefix, 155);
+						strcat(namebuf, "/");
+						strcat(namebuf, head.name);
+				}
+				else{
+					namebuf = strdup(head.name);
+				}
+				size = strdup(head.size);
+				amount = octtodec(atoi(size));
+			}
+
+
+			/*if(namebuf[0]!= 0){
 				createnewfile(buff, namebuf, readfile, amount);
-			}/*
-			else{
-				lseek(readfile, amount, SEEK_CUR);
-			}*/
+			}
 
 			 numblocks = amount / 512;
 			 remainder = amount % 512;
@@ -63,74 +130,22 @@ void extract(int readfile, char *name){
 				numblocks++;
 			}
 
-			/*still need to read the rest of stuff if over 512*/
 			memset(buff, 0 ,512);
-			lseek(readfile, numblocks * 512 - amount, SEEK_CUR);
-		}
-
-
-	}else{
-	while(read(readfile, buff, 512)>0){
-		/*printf("header: %s\n", buff);*/
-		memset(namebuf, 0 ,255);
-		for(i = 0; i< 100; i++){
-			namebuf[i] = buff[i];
-		}
-		if(namebuf[99]!= '\0'){
-			i = 345;
-			j = 100;
-			while(buff[i] != '\0' && i< 500){
-				namebuf[j] = buff[i];
-				i++;
-				j++;
-			}
-			
-			namebuf[j] = '\0';
-			
-		}
-		for(i = 0, j = 124; i<12; i++, j++){
-			size[i] = buff[j];
-		}
-		amount = octtodec(atoi(size));
-		if(namebuf[0]!= 0){
-			createnewfile(buff, namebuf, readfile, amount);
-		}
-		 numblocks = amount / 512;
-		 remainder = amount % 512;
-
-		if(remainder!=0){
-			numblocks++;
-		}
-
-		memset(buff, 0 ,512);
-		lseek(readfile, numblocks * 512-amount, SEEK_CUR);
+			lseek(readfile, numblocks * 512-amount, SEEK_CUR);*/
 		}
 	}
 }
 
 /*makes new thing to write to*/
-void createnewfile(char buff[512], char namebuf[256],
-                    int readfile, int amount){
-	int newfile;
+void createnewfile(char namebuf[256], int readfile, int amount){
+	int newfile, count;
 	char ch;
-	int curr = 0;
-	char mod[8];
-	char *ptr = NULL;
-	mode_t mode = 0;
-	int i = 100;
-	int j = 0;
-	while(buff[i] != 0){
-		mod[j] = buff[i];
-		i++;
-		j++;
-	}
-	mod[j] = 0;		
-	mode = (mode_t)strtol(mod, &ptr, 8);
+
 	newfile = open(namebuf, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	while(curr < amount){
-		read(readfile, &ch, 1);
-		write(newfile, &ch, 1);
-		curr++;
+	while(count < amount){
+		read(readfile, ch, 1);
+		write(newfile, ch, 1);
+		count++;
 	}
 }
 
